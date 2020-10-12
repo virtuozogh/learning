96c067ce965f3ac88d7255d4fa18215c8e8a4d28
virtuozogh <mark.v@raya.edu.ph>
2020-10-12 12:50:09 +0800

    added local key map
    trying to do smooth movement feature with using delta time. Currently only crashes. I will research.

--- 1.5 l2d actual movement/main.lua
+++ 1.5 l2d actual movement/main.lua
@@ -15,32 +15,45 @@ x3,y3,
 x4,y4}
   
   love.graphics.polygon("fill", square)
+  love.graphics.print(seconds, 10, 10)
 end
 
-love.keypressed = function(pressed_key)
-  if pressed_key == "w" then
+local key_map = {
+  w = function()
     y1 = y1 - 10
     y2 = y2 - 10
     y3 = y3 - 10
     y4 = y4 - 10
-  elseif pressed_key == "a" then
+  end,
+  a = function()
     x1 = x1 - 10
     x2 = x2 - 10
     x3 = x3 - 10
     x4 = x4 - 10
-  elseif pressed_key == "d" then
-    x1 = x1 + 10
-    x2 = x2 + 10
-    x3 = x3 + 10
-    x4 = x4 + 10
-  elseif pressed_key == "s" then
+  end,
+  s = function()
     y1 = y1 + 10
     y2 = y2 + 10
     y3 = y3 + 10
     y4 = y4 + 10
+  end,
+  d = function()
+    x1 = x1 + 10
+    x2 = x2 + 10
+    x3 = x3 + 10
+    x4 = x4 + 10
+  end
+}
+
+love.keypressed = function(pressed_key)
+  if key_map[pressed_key] then
+    key_map[pressed_key]()
   end
 end
 
 love.update = function(dt)
-  seconds = seconds + dt
+  seconds = dt
+  if dt == 0.1 then
+    dt = 0
+  end
 end
